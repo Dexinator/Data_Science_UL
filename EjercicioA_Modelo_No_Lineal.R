eca529de3aac74282cce7031948cd68c0e77b7e8
Jorge Badillo <badillo.delgado.jorge@gmail.com>
2024-01-04 19:12:09 -0600

    Se terminó de comentar el ejericio A regresión no lineal

--- EjercicioA_Modelo_No_Lineal.R
+++ EjercicioA_Modelo_No_Lineal.R
@@ -1,43 +1,67 @@
 library(caret)
+library(dplyr)
+library(arm)
+library(kernlab)
+
+#importamos los datos, en caso no los tengamos aún
 dataset<-read.csv("A:\\Downloads\\Prueba\\Examen\\Tooltest-dataset.csv", header=TRUE)
+
+#Quitaremos la variable density, pues como vimos en el ejercicio anterior tiene correlaciones altas con más
+#variables. Además, haremos un análisis de correlación tras quitarla para comprobar que no exista alguna más
+dataset <- subset(dataset,select=-density)
 head(dataset)
-dim(dataset)
+correlation_0 = round(cor(dataset), 2)
+ggcorrplot(correlation_0, hc.order = TRUE, type = "lower",
+           lab = TRUE)
+#No tenemos ninguna correlación arriba de .75
+
+#Comenzamos con el modelo logístico, este modelo puede ser útil para estimar valores entre 0 y 1. Además
+#cuando un valor es muy grande (positivo o negativo), tiende a 1 o a cero (respectivamente).
 log_model <- glm(Target ~ fixed.acidity + volatile.acidity + citric.acid + residual.sugar + chlorides +
-                   free.sulfur.dioxide + total.sulfur.dioxide + density + pH + sulphates + alcohol, data = dataset, family = "binomial")
-
+                   free.sulfur.dioxide + total.sulfur.dioxide + pH + sulphates + alcohol, data = dataset, family = "binomial")
 summary(log_model)
+#Vemos que el p-values de fixed.acidity es mayor a .05 por lo que no es estadísticamente significante
+#Lo quitaremos y comprobaremos si el modelo mejora
 
+#Modelo logístico 2, quitamos la variable fixed.acidity
+log_model2 <- glm(Target ~  volatile.acidity + citric.acid + residual.sugar + chlorides +
+                  free.sulfur.dioxide + total.sulfur.dioxide + pH + sulphates + alcohol, data = dataset, family = "binomial")
+log_model$df.residual
+log_model2$df.residual
+log_model$null.deviance
+log_model2$null.deviance
+log_model$aic
+log_model2$aic
+#No hay suficiente evidencia de una mejora significativa con estos indicadores. Tampoco de algo peor.
 
-log_model2 <- glm(Target ~ fixed.acidity + volatile.acidity + residual.sugar + chlorides +
-                   free.sulfur.dioxide + density + pH + sulphates, data = dataset, family = "binomial")
-summary(log_model2)
+#Para comprobar cuál de los dos es mejor, haremos una prueba dividiendo nuestro dataset en train(80%) y test(20%)
+#Con esto podríamos ver cuál de los dos se desempeña mejor con la variable dependiente.
+split = round(nrow(dataset) * 0.80)
+Train_Data=dataset[1:split, ]
+Test_Data=dataset[(split + 1):nrow(dataset), ]
+log_model <- glm(Target ~ fixed.acidity + volatile.acidity + citric.acid + residual.sugar + chlorides +
+                   free.sulfur.dioxide + total.sulfur.dioxide + pH + sulphates + alcohol, data = Train_Data, family = "binomial")
+Test_Data$model_prob <- predict(log_model, Test_Data, type = "response")
+Test_Data <- Test_Data  %>% mutate(model_pred = 1*(model_prob > .5) + 0,
+                         target_binary = 1*(Target = 1) + 0)
+head(Test_Data)
+Test_Data <- Test_Data %>% mutate(accurate = 1*(model_pred == Target))
+Acclogmodel=sum(Test_Data$accurate)/nrow(Test_Data)
+#Tras darle solo el 80% de los datos a log_model cuando probamos este modelo con el 20% restante
+#Tenemos un accuracy de 78.26%
+log_model2 <- glm(Target ~  volatile.acidity + citric.acid + residual.sugar + chlorides +
+                    free.sulfur.dioxide + total.sulfur.dioxide + pH + sulphates + alcohol, data = Train_Data, family = "binomial")
+Test_Data$model_prob2 <- predict(log_model2, Test_Data, type = "response")
+Test_Data <- Test_Data  %>% mutate(model_pred2 = 1*(model_prob2 > .5) + 0,
+                                   target_binary = 1*(Target = 1) + 0)
+head(Test_Data)
+Test_Data <- Test_Data %>% mutate(accurate2 = 1*(model_pred2 == Target))
+Acclogmodel2=sum(Test_Data$accurate2)/nrow(Test_Data)
+#Hicimos lo mismo con el log_model2 y tenemos un accuracy 78.67
+#Es una mejora muy pequeña pero al comparar los modelos también tenemos más certeza ya de que funcionan
 
-
-install.packages("arm")
-library("arm")
-
-cv_model1 <- train(
-  as.factor(Target) ~ fixed.acidity + volatile.acidity + residual.sugar + chlorides +
-    free.sulfur.dioxide + density + pH + sulphates, data = dataset,   method = "glm",family = "binomial")
-
-cv_model1$results$Accuracy
-cv_model1$results
-
-cv_model2 <- train(
-  Attrition ~ MonthlyIncome + OverTime, 
-  data = churn_train, 
-  method = "glm",
-  family = "binomial",
-  trControl = trainControl(method = "cv", number = 10)
-)
-
-cv_model1$pred
-
-
-summary(model1 = cv_model1)
-resamples(cv_model1)
-
-
+#Con un Binned Residual Plot podemos crear bandas de confianza para modelos logísticos y comprobar de una
+#manera visual que la mayoría de nuestros residuos están dentro de ellas
 binnedplot(fitted(log_model2), 
            residuals(log_model2, type = "response"), 
            nclass = NULL, 
@@ -47,35 +71,4 @@ binnedplot(fitted(log_model2),
            cex.pts = 0.8, 
            col.pts = 1, 
            col.int = "gray")
-
-
-install.packages("lmtest")
-library("lmtest")
-lrtest(log_model, log_model2)
-
-install.packages("caret")
-library("caret")
-confusionMatrix(as.factor(ifelse(fitted(log_model) > .5, "1", "0")), as.factor(dataset$Target))$table
-confusionMatrix(as.factor(ifelse(fitted(log_model2) > .5, "1", "0")), as.factor(dataset$Target))$table
-
-#Accuracy Comparisions
-#Modelo 1
-1-((763+205)/(3633+763+205+297))
-#Modelo 2
-1-((766+215)/(3623+766+215+294))
-
-install.packages("pROC")
-library("pROC")
-
-invisible(plot(roc(dataset$Target,
-                   fitted(log_model)),
-               col = "red", 
-               main = "ROC curves: logistic model 1 (red) vs. logistic model 2 (blue)"))
-
-invisible(plot(roc(dataset$Target,
-                   fitted(log_model2)),
-               print.auc = T, 
-               col = "blue", 
-               add = T))
-
 
